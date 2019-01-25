788d94d5b1df950385619d57d55dc3f1d91c7e10
EC2 Default User <ec2-user@ip-172-31-16-9.us-east-2.compute.internal>
2019-01-25 13:37:54 +0000

    resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :companies
-  devise_for :users
+    root to: 'home#top'
+  devise_for :companies, controllers: {
+    registrations: 'companies/registrations',
+    sessions: "companies/sessions",
+  }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
